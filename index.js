8c77df27001b06d886a00c92c33f54b9aac2dc69
Kumail786 <kumailrawjani900@gmail.com>
2019-12-14 14:41:19 +0500

    initial setup and get all notes done

---
+++ index.js
@@ -0,0 +1,16 @@
+const grpc = require('grpc')
+const notesProto = grpc.load('./notes.proto')
+const server = new grpc.Server()
+
+const notes = [
+    { id: '1', title: 'Note 1', content: 'Content 1'},
+    { id: '2', title: 'Note 2', content: 'Content 2'}
+]
+server.addService(notesProto.NoteService.service, {
+    list: (_, callback) => {
+        callback(null, notes)
+    },
+})
+server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure())
+console.log('Server running at http://127.0.0.1:50051')
+server.start()
