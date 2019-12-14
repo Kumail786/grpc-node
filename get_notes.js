4c7d6925720fa12260113e8ce8ada52acee418e3
Kumail786 <kumailrawjani900@gmail.com>
2019-12-14 14:58:16 +0500

    add note done

--- get_notes.js
+++ get_notes.js
@@ -1,9 +1,21 @@
-const client = require('./client')
+const client = require("./client");
 client.list({}, (error, notes) => {
-    if (!error) {
-        console.log('successfully fetch List notes')
-        console.log(notes)
-    } else {
-        console.error(error)
-    }
-})
+  if (!error) {
+    console.log("successfully fetch List notes");
+    console.log(notes);
+  } else {
+    console.error(error);
+  }
+});
+const data = {
+  title: "Kumail",
+  content: "hello world"
+};
+client.insert(data, (error, note) => {
+  if (!error) {
+    console.log("successfully added data");
+    console.log(note)
+  } else {
+    console.log("error");
+  }
+});
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const grpc = require('grpc')
 const notesProto = grpc.load('./notes.proto')
 const server = new grpc.Server()
+const uuidv1 = require('uuid/v1')
 
 const notes = [
     { id: '1', title: 'Note 1', content: 'Content 1'},
@@ -10,6 +11,12 @@ server.addService(notesProto.NoteService.service, {
     list: (_, callback) => {
         callback(null, notes)
     },
+    insert: (call, callback) => {
+        let note = call.request
+        note.id = uuidv1()
+        notes.push(note)
+        callback(null, note)
+    }
 })
 server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure())
 console.log('Server running at http://127.0.0.1:50051')
